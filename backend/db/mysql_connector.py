58f6f2a38ae2cb0600cfed7784f306c32d6a0af5
Harsh Rana <117711346+rHarsh-11@users.noreply.github.com>
2025-07-25 19:22:29 +0530

    Update mysql_connector.py

--- backend/db/mysql_connector.py
+++ backend/db/mysql_connector.py
@@ -1,17 +1,31 @@
+import os
 import mysql.connector
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# MySQL connection setup
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
 conn = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="rootpass",
-    database="valuefy"
+    host=MYSQL_HOST,
+    port=MYSQL_PORT,
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    database=MYSQL_DATABASE
 )
 
 def fetch_from_mysql(query: str):
+    """
+    Execute a SQL query on the MySQL database and return the result as a Pandas DataFrame.
+    """
     cursor = conn.cursor(dictionary=True)
     cursor.execute(query)
     result = cursor.fetchall()
-    df = pd.DataFrame(result)
-    return df
+    cursor.close()
+    return pd.DataFrame(result)
